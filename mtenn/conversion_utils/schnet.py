c586b26afd9d37f9640834c09290746bec602564
kaminow <benjamin.kaminow@choderalab.org>
2022-06-24 15:28:27 -0400

    Add SchNet subclass to handle unpacking passed data arguments.

--- mtenn/conversion_utils/schnet.py
+++ mtenn/conversion_utils/schnet.py
@@ -3,21 +3,40 @@ Representation and strategy for SchNet model.
 """
 from copy import deepcopy
 import torch
+from torch_geometric.nn.models import SchNet as PygSchNet
 
 from ..model import ConcatStrategy, DeltaStrategy, Model
 
-def get_reprentation(model):
+class SchNet(PygSchNet):
+    def __init__(self, model=None):
+        ## If no model is passed, construct default SchNet model, otherwise copy
+        ##  all parameters and weights over
+        if model is None:
+            super(SchNet, self).__init__()
+        else:
+            atomref = model.atomref.weight.detach().clone()
+            model_params = (model.hidden_channels, model.num_filters,
+                model.num_interactions, model.num_gaussians,
+                model.cutoff, model.max_num_neighbors,model.readout,
+                model.dipole, model.mean, model.std, atomref)
+            super(SchNet, self).__init__(*model_params)
+            self.load_state_dict(model.state_dict())
+
+    def forward(self, data):
+        return(super(SchNet, self).forward(data[0], data[1]))
+
+def get_representation(model):
     """
     Input model, remove last layer.
 
     Parameters
     ----------
-    model: torch_geometric.nn.models.SchNet
-        pyg SchNet model
+    model: SchNet
+        SchNet model
 
     Returns
     -------
-    torch_geometric.nn.models.SchNet
+    SchNet
         Copied SchNet model with the last layer replaced by an Identity module
     """
 
@@ -34,8 +53,8 @@ def get_energy_func(model):
 
     Parameters
     ----------
-    model: torch_geometric.nn.models.SchNet
-        pyg SchNet model
+    model: SchNet
+        SchNet model
 
     Returns
     -------
@@ -51,8 +70,8 @@ def get_delta_strategy(model):
 
     Parameters
     ----------
-    model: torch_geometric.nn.models.SchNet
-        pyg SchNet model
+    model: SchNet
+        SchNet model
 
     Returns
     -------
@@ -68,8 +87,8 @@ def get_concat_strategy(model):
 
     Parameters
     ----------
-    model: torch_geometric.nn.models.SchNet
-        pyg SchNet model
+    model: SchNet
+        SchNet model
 
     Returns
     -------
@@ -80,7 +99,7 @@ def get_concat_strategy(model):
 
 def get_model(model, strategy: str='delta'):
     ## First get representation module
-    representation = get_reprentation(model)
+    representation = get_representation(model)
 
     ## Construct strategy module based on model and
     ##  representation (if necessary)
