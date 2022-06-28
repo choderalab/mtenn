5b6b004f0831521303127ebc542d47f74c9186f4
kaminow <benjamin.kaminow@choderalab.org>
2022-06-28 11:16:24 -0400

    Changed SchNet Model building to a class-based interface.

--- mtenn/conversion_utils/schnet.py
+++ mtenn/conversion_utils/schnet.py
@@ -25,87 +25,93 @@ class SchNet(PygSchNet):
     def forward(self, data):
         return(super(SchNet, self).forward(data[0], data[1]))
 
-def get_representation(model):
-    """
-    Input model, remove last layer.
+    def _get_representation(self):
+        """
+        Input model, remove last layer.
 
-    Parameters
-    ----------
-    model: SchNet
-        SchNet model
+        Parameters
+        ----------
+        model: SchNet
+            SchNet model
 
-    Returns
-    -------
-    SchNet
-        Copied SchNet model with the last layer replaced by an Identity module
-    """
+        Returns
+        -------
+        SchNet
+            Copied SchNet model with the last layer replaced by an Identity module
+        """
 
-    ## Copy model so initial model isn't affected
-    model_copy = deepcopy(model)
-    ## Replace final linear layer with an identity module
-    model_copy.lin2 = torch.nn.Identity()
+        ## Copy model so initial model isn't affected
+        model_copy = deepcopy(self)
+        ## Replace final linear layer with an identity module
+        model_copy.lin2 = torch.nn.Identity()
 
-    return(model_copy)
+        return(model_copy)
 
-def get_energy_func(model):
-    """
-    Return last layer of the model.
+    def _get_energy_func(self):
+        """
+        Return last layer of the model.
 
-    Parameters
-    ----------
-    model: SchNet
-        SchNet model
+        Parameters
+        ----------
+        model: SchNet
+            SchNet model
 
-    Returns
-    -------
-    torch.nn.modules.linear.Linear
-        Copy of `model`'s last layer
-    """
+        Returns
+        -------
+        torch.nn.modules.linear.Linear
+            Copy of `model`'s last layer
+        """
 
-    return(deepcopy(model.lin2))
+        return(deepcopy(self.lin2))
 
-def get_delta_strategy(model):
-    """
-    Build a DeltaStrategy object based on the passed model.
+    def _get_delta_strategy(self):
+        """
+        Build a DeltaStrategy object based on the passed model.
 
-    Parameters
-    ----------
-    model: SchNet
-        SchNet model
+        Parameters
+        ----------
+        model: SchNet
+            SchNet model
 
-    Returns
-    -------
-    DeltaStrategy
-        DeltaStrategy built from `model`
-    """
+        Returns
+        -------
+        DeltaStrategy
+            DeltaStrategy built from `model`
+        """
 
-    return(DeltaStrategy(get_energy_func(model)))
+        return(DeltaStrategy(self._get_energy_func()))
 
-def get_concat_strategy(model):
-    """
-    Build a ConcatStrategy object based on the passed model.
+    @staticmethod
+    def get_model(model=None, strategy: str='delta'):
+        """
+        Exposed function to build a Model object from a SchNet object. If none
+        is provided, a default model is initialized.
 
-    Parameters
-    ----------
-    model: SchNet
-        SchNet model
+        Parameters
+        ----------
+        model: SchNet, optional
+            SchNet model to use to build the Model object. If left as none, a
+            default model will be initialized and used
+        strategy: str, default='delta'
+            Strategy to use to combine representation of the different parts.
+            Options are ['delta', 'concat']
 
-    Returns
-    -------
-    ConcatStrategy
-        ConcatStrategy built from `model`
-    """
-    pass
+        Returns
+        -------
+        Model
+            Model object containing the desired Representation and Strategy
+        """
+        if model is None:
+            model = SchNet()
 
-def get_model(model, strategy: str='delta'):
-    ## First get representation module
-    representation = get_representation(model)
+        ## First get representation module
+        representation = model._get_representation()
 
-    ## Construct strategy module based on model and
-    ##  representation (if necessary)
-    if strategy == 'delta':
-        strategy = get_delta_strategy(model)
-    elif strategy == 'concat':
-        strategy = ConcatStrategy()
+        ## Construct strategy module based on model and
+        ##  representation (if necessary)
+        if strategy == 'delta':
+            strategy = model._get_delta_strategy()
+        elif strategy == 'concat':
+            strategy = ConcatStrategy()
 
-    return(Model(representation, strategy))
+        return(Model(representation, strategy))
