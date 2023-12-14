0f8c9de00dd2660a8d6b8fcc60132b8f94cbae34
kaminow <benjamin.kaminow@choderalab.org>
2023-12-14 10:59:15 -0500

    Remove duplicated code.

--- mtenn/config.py
+++ mtenn/config.py
@@ -37,18 +37,6 @@ class ModelType(StringEnum):
     e3nn = "e3nn"
     INVALID = "INVALID"
 
-    @classmethod
-    def get_values(cls) -> list[str]:
-        return [member.value for member in cls]
-
-    @classmethod
-    def reverse_lookup(cls, value):
-        return cls(value)
-
-    @classmethod
-    def get_names(cls) -> list[str]:
-        return [member.name for member in cls]
-
 
 class StrategyConfig(StringEnum):
     """
