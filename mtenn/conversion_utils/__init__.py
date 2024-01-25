6a0d11155ac6e2d72f76ed5c586257e61f00e5dd
fyng <feiyangthegoat@gmail.com>
2024-01-25 11:56:26 -0500

    add visnet to init

--- mtenn/conversion_utils/__init__.py
+++ mtenn/conversion_utils/__init__.py
@@ -1,5 +1,6 @@
 from .e3nn import E3NN
 from .gat import GAT
 from .schnet import SchNet
+from .visnet import ViSNet
 
-__all__ = ["E3NN", "GAT", "SchNet"]
+__all__ = ["E3NN", "GAT", "SchNet", "ViSNet"]
