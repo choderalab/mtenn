f78ed8c6b8fd047bfdb2cefa0c3a88958c48229f
fyng <feiyangthegoat@gmail.com>
2024-01-29 11:22:25 -0500

    add test for visnet

--- mtenn/tests/test_model_config.py
+++ mtenn/tests/test_model_config.py
@@ -1,4 +1,4 @@
-from mtenn.config import GATModelConfig, E3NNModelConfig, SchNetModelConfig
+from mtenn.config import GATModelConfig, E3NNModelConfig, SchNetModelConfig, ViSNetModelConfig
 
 
 def test_random_seed_gat():
@@ -48,6 +48,27 @@ def test_random_seed_e3nn():
 def test_random_seed_schnet():
     rand_config = SchNetModelConfig()
     set_config = SchNetModelConfig(rand_seed=10)
+
+    rand_model1 = rand_config.build()
+    rand_model2 = rand_config.build()
+    set_model1 = set_config.build()
+    set_model2 = set_config.build()
+
+    rand_equal = [
+        (p1 == p2).all()
+        for p1, p2 in zip(rand_model1.parameters(), rand_model2.parameters())
+    ]
+    assert sum(rand_equal) < len(rand_equal)
+
+    set_equal = [
+        (p1 == p2).all()
+        for p1, p2 in zip(set_model1.parameters(), set_model2.parameters())
+    ]
+    assert sum(set_equal) == len(set_equal)
+
+def test_random_seed_visnet():
+    rand_config = ViSNetModelConfig()
+    set_config = ViSNetModelConfig(rand_seed=10)
 
     rand_model1 = rand_config.build()
     rand_model2 = rand_config.build()
