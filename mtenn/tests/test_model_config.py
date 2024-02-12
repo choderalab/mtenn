00a90b4aced6068daa13de9c1caa2f9b42ad2c4f
kaminow <kaminow@cshl.edu>
2024-02-12 11:51:01 -0500

    Add tests for setting pred_readout via model configs.

--- mtenn/tests/test_model_config.py
+++ mtenn/tests/test_model_config.py
@@ -1,4 +1,7 @@
+import pytest
+
 from mtenn.config import GATModelConfig, E3NNModelConfig, SchNetModelConfig
+from mtenn.readout import PIC50Readout, PKiReadout
 
 
 def test_random_seed_gat():
@@ -23,6 +26,32 @@ def test_random_seed_gat():
     assert sum(set_equal) == len(set_equal)
 
 
+@pytest.mark.parametrize(
+    "pred_r,pred_r_class,pred_r_args",
+    [
+        (None, None, [None, None]),
+        ("pic50", PIC50Readout, [None, None]),
+        ("pic50", PIC50Readout, [0.375, 9.5]),
+        ("pki", PKiReadout, [None, None]),
+    ],
+)
+def test_readout_gat(pred_r, pred_r_class, pred_r_args):
+    model = GATModelConfig(
+        pred_readout=pred_r,
+        pred_substrate=pred_r_args[0],
+        pred_km=pred_r_args[1],
+    ).build()
+
+    if pred_r is None:
+        assert model.readout is None
+        return
+
+    assert isinstance(model.readout, pred_r_class)
+    if pred_r == "pic50":
+        assert model.readout.substrate == pred_r_args[0]
+        assert model.readout.Km == pred_r_args[1]
+
+
 def test_random_seed_e3nn():
     rand_config = E3NNModelConfig()
     set_config = E3NNModelConfig(rand_seed=10)
@@ -45,6 +74,32 @@ def test_random_seed_e3nn():
     assert sum(set_equal) == len(set_equal)
 
 
+@pytest.mark.parametrize(
+    "pred_r,pred_r_class,pred_r_args",
+    [
+        (None, None, [None, None]),
+        ("pic50", PIC50Readout, [None, None]),
+        ("pic50", PIC50Readout, [0.375, 9.5]),
+        ("pki", PKiReadout, [None, None]),
+    ],
+)
+def test_readout_e3nn(pred_r, pred_r_class, pred_r_args):
+    model = E3NNModelConfig(
+        pred_readout=pred_r,
+        pred_substrate=pred_r_args[0],
+        pred_km=pred_r_args[1],
+    ).build()
+
+    if pred_r is None:
+        assert model.readout is None
+        return
+
+    assert isinstance(model.readout, pred_r_class)
+    if pred_r == "pic50":
+        assert model.readout.substrate == pred_r_args[0]
+        assert model.readout.Km == pred_r_args[1]
+
+
 def test_random_seed_schnet():
     rand_config = SchNetModelConfig()
     set_config = SchNetModelConfig(rand_seed=10)
@@ -65,3 +120,29 @@ def test_random_seed_schnet():
         for p1, p2 in zip(set_model1.parameters(), set_model2.parameters())
     ]
     assert sum(set_equal) == len(set_equal)
+
+
+@pytest.mark.parametrize(
+    "pred_r,pred_r_class,pred_r_args",
+    [
+        (None, None, [None, None]),
+        ("pic50", PIC50Readout, [None, None]),
+        ("pic50", PIC50Readout, [0.375, 9.5]),
+        ("pki", PKiReadout, [None, None]),
+    ],
+)
+def test_readout_schnet(pred_r, pred_r_class, pred_r_args):
+    model = SchNetModelConfig(
+        pred_readout=pred_r,
+        pred_substrate=pred_r_args[0],
+        pred_km=pred_r_args[1],
+    ).build()
+
+    if pred_r is None:
+        assert model.readout is None
+        return
+
+    assert isinstance(model.readout, pred_r_class)
+    if pred_r == "pic50":
+        assert model.readout.substrate == pred_r_args[0]
+        assert model.readout.Km == pred_r_args[1]
