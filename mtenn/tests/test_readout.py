4d33442e2f6692c3c66ceae7248d84a853627cf0
kaminow <kaminow@cshl.edu>
2024-02-12 15:39:37 -0500

    Add PIC50Readout tests.

---
+++ mtenn/tests/test_readout.py
@@ -0,0 +1,44 @@
+import pytest
+import torch
+
+from mtenn.readout import PIC50Readout, PKiReadout
+
+
+@pytest.fixture
+def values():
+    cp_values = [0.375, 9.5]
+
+    # Input dG value
+    dG = torch.tensor(-5, dtype=float)
+    # No Cheng-Prusoff equation
+    raw_pic50 = -dG / torch.log(torch.tensor(10))
+    # Using Cheng-Prusoff
+    cp_pic50 = raw_pic50 - torch.log10(
+        torch.tensor(1 + cp_values[0] / cp_values[1], dtype=raw_pic50.dtype)
+    )
+
+    return cp_values, dG, raw_pic50, cp_pic50
+
+
+def test_pic50_readout_repr():
+    r = PIC50Readout(substrate=10, Km=10)
+    assert repr(r) == "PIC50Readout(substrate=10, Km=10)"
+
+
+def test_pic50_readout_str():
+    r = PIC50Readout(substrate=10, Km=10)
+    assert str(r) == "PIC50Readout(substrate=10, Km=10)"
+
+
+def test_pic50_readout_no_cheng_prusoff(values):
+    r = PIC50Readout()
+    _, dG, raw_pic50, _ = values
+
+    assert r(dG) == raw_pic50
+
+
+def test_pic50_readout_cheng_prusoff(values):
+    cp_values, dG, _, cp_pic50 = values
+    r = PIC50Readout(*cp_values)
+
+    assert torch.isclose(r(dG), cp_pic50)
