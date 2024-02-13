489b46110a3ebdf24c30f5f3663cecf47b061ee9
kaminow <kaminow@cshl.edu>
2024-02-13 15:47:27 -0500

    Finish converseion_utils.GAT tests.

--- mtenn/tests/test_gat.py
+++ mtenn/tests/test_gat.py
@@ -1,6 +1,19 @@
+import pytest
+import torch
+
 from dgllife.model import GAT as GAT_dgl
 from dgllife.utils import CanonicalAtomFeaturizer, SMILESToBigraph
 from mtenn.conversion_utils.gat import GAT
+
+
+@pytest.fixture
+def model_input():
+    smiles = "CCCC"
+    g = SMILESToBigraph(add_self_loop=True, node_featurizer=CanonicalAtomFeaturizer())(
+        smiles
+    )
+
+    return {"g": g, "smiles": smiles}
 
 
 def test_build_gat_directly_kwargs():
@@ -46,3 +59,36 @@ def test_build_gat_from_dgl_gat():
 def test_set_predictor_hidden_feats():
     model = GAT(in_feats=10, predictor_hidden_feats=10)
     assert model.predict[0].out_features == 10
+
+
+def test_gat_can_predict(model_input):
+    model = GAT(in_feats=CanonicalAtomFeaturizer().feat_size())
+    _ = model(model_input)
+
+
+def test_representation_is_correct():
+    model = GAT(in_feats=10)
+    rep = model._get_representation()
+
+    model_params = dict(model.gnn.named_parameters())
+    for n, rep_param in rep.named_parameters():
+        assert torch.allclose(rep_param, model_params[n])
+
+
+def test_get_model_no_ref():
+    model = GAT.get_model(in_feats=10)
+
+    assert isinstance(model.representation, GAT)
+    assert model.readout is None
+
+
+def test_get_model_ref():
+    ref_model = GAT(in_feats=10)
+    model = GAT.get_model(model=ref_model)
+
+    assert isinstance(model.representation, GAT)
+    assert model.readout is None
+
+    ref_params = dict(ref_model.named_parameters())
+    for n, model_param in model.representation.named_parameters():
+        assert torch.allclose(model_param, ref_params[n])
