c276ade0d95756608a830f9bdb8d550b4eec9ff8
kaminow <kaminow@cshl.edu>
2024-04-18 15:50:16 -0400

    Start working on conversion_utils.visnet docstrings.

--- mtenn/conversion_utils/visnet.py
+++ mtenn/conversion_utils/visnet.py
@@ -1,10 +1,13 @@
 """
-Representation and strategy for ViSNet model.
+``Representation`` and ``Strategy`` implementations for the ViSNet model architecture.
+The underlying model that we use is the implementation in
+`PyTorch Geometric <https://pytorch-geometric.readthedocs.io/en/latest/generated/
+torch_geometric.nn.models.ViSNet.html#torch_geometric.nn.models.ViSNet>`_.
 """
 from copy import deepcopy
 import torch
 from torch_geometric.utils import scatter
-from torch_geometric.nn.models import ViSNet as PygVisNet
+from torch_geometric.nn.models import ViSNet as PygViSNet
 from torch_geometric.nn.models.visnet import ViS_MP_Vertex
 
 from mtenn.model import GroupedModel, Model
@@ -12,11 +15,26 @@ from mtenn.strategy import ComplexOnlyStrategy, ConcatStrategy, DeltaStrategy
 
 
 class EquivariantVecToScalar(torch.nn.Module):
-    # Wrapper for PygVisNet.EquivariantScalar to implement forward() method
+    """
+    Wrapper of torch_geometric.nn.models.visnet.EquivariantScalar to implement
+    ``forward`` method.
+    """
+
     def __init__(self, mean, reduce_op):
+        """
+        Store use parameters.
+
+        Parameters
+        ----------
+        mean : torch.Tensor
+            Mean of predicted value
+        reduce_op : str
+            Reduce operation to use in torch_geometric.utils._scatter.scatter
+        """
         super(EquivariantVecToScalar, self).__init__()
         self.mean = mean
         self.reduce_op = reduce_op
+
     def forward(self, x):
         # dummy variable. all atoms from the same molecule and the same batch
         batch = torch.zeros(x.shape[0], dtype=torch.int64, device=x.device)
@@ -25,34 +43,42 @@ class EquivariantVecToScalar(torch.nn.Module):
 
 
 class ViSNet(torch.nn.Module):
+    """
+    ``mtenn`` wrapper around the PyTorch Geometric ViSNet model. This class handles
+    construction of the model and the formatting into ``Representation`` and
+    ``Strategy`` blocks.
+    """
+
     def __init__(self, *args, model=None, **kwargs):
         super().__init__()
         ## If no model is passed, construct default ViSNet model, otherwise copy
         ##  all parameters and weights over
         if model is None:
-            self.visnet = PygVisNet(*args, **kwargs)
+            self.visnet = PygViSNet(*args, **kwargs)
         else:
             atomref = model.prior_model.atomref.weight.detach().clone()
             model_params = {
-                'lmax': model.representation_model.lmax,
-                'vecnorm_type': model.representation_model.vecnorm_type,
-                'trainable_vecnorm': model.representation_model.trainable_vecnorm,
-                'num_heads': model.representation_model.num_heads,
-                'num_layers': model.representation_model.num_layers,
-                'hidden_channels': model.representation_model.hidden_channels,
-                'num_rbf': model.representation_model.num_rbf,
-                'trainable_rbf': model.representation_model.trainable_rbf,
-                'max_z': model.representation_model.max_z,
-                'cutoff': model.representation_model.cutoff,
-                'reduce_op': model.representation_model.max_num_neighbors,
-                'vertex': isinstance(model.representation_model.vis_mp_layers[0], ViS_MP_Vertex),
-                'reduce_op': model.reduce_op,
-                'mean': model.mean,
-                'std': model.std,
-                'derivative': model.derivative, # not used. originally calculates "force" from energy
-                'atomref': atomref,
+                "lmax": model.representation_model.lmax,
+                "vecnorm_type": model.representation_model.vecnorm_type,
+                "trainable_vecnorm": model.representation_model.trainable_vecnorm,
+                "num_heads": model.representation_model.num_heads,
+                "num_layers": model.representation_model.num_layers,
+                "hidden_channels": model.representation_model.hidden_channels,
+                "num_rbf": model.representation_model.num_rbf,
+                "trainable_rbf": model.representation_model.trainable_rbf,
+                "max_z": model.representation_model.max_z,
+                "cutoff": model.representation_model.cutoff,
+                "reduce_op": model.representation_model.max_num_neighbors,
+                "vertex": isinstance(
+                    model.representation_model.vis_mp_layers[0], ViS_MP_Vertex
+                ),
+                "reduce_op": model.reduce_op,
+                "mean": model.mean,
+                "std": model.std,
+                "derivative": model.derivative,  # not used. originally calculates "force" from energy
+                "atomref": atomref,
             }
-            self.visnet = PygVisNet(**model_params)
+            self.visnet = PygViSNet(**model_params)
             self.visnet.load_state_dict(model.state_dict())
 
         self.readout = EquivariantVecToScalar(self.visnet.mean, self.visnet.reduce_op)
@@ -75,7 +101,7 @@ class ViSNet(torch.nn.Module):
 
         Notes
         -----
-        Assumes all atoms passed in `data` belong to the same molecule and are processed in a single batch. 
+        Assumes all atoms passed in `data` belong to the same molecule and are processed in a single batch.
         """
         pos = data["pos"]
         z = data["z"]
