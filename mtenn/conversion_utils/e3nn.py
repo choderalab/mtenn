3ef51d8f6e79caee5f547ff3b1f1cd79385c49e3
kaminow <kaminow@cshl.edu>
2024-04-18 15:11:50 -0400

    Polish docstrings in conversion_utils.e3nn.

--- mtenn/conversion_utils/e3nn.py
+++ mtenn/conversion_utils/e3nn.py
@@ -1,5 +1,8 @@
 """
-Representation and strategy for e3nn model.
+``Representation`` and ``Strategy`` implementations for an E(3)-equivariant model using
+`e3nn <https://docs.e3nn.org/en/latest/index.html>`_. The underlying model that we use
+is specifically the `January 2021 Network
+<https://docs.e3nn.org/en/latest/api/nn/models/gate_points_2101.html>`_ model.
 """
 from copy import deepcopy
 import torch
@@ -10,9 +13,27 @@ from mtenn.strategy import ComplexOnlyStrategy, ConcatStrategy, DeltaStrategy
 
 
 class E3NN(Network):
+    """
+    ``mtenn`` wrapper around the e3nn model. This class handles construction of the
+    model and the formatting into ``Representation`` and ``Strategy`` blocks.
+    """
+
     def __init__(self, *args, model=None, **kwargs):
-        ## If no model is passed, construct E3NN model with model_kwargs,
-        ##  otherwise copy all parameters and weights over
+        """
+        Initialize the underlying ``e3nn.nn.models.gate_points_2101.Network`` model. If
+        a value is passed for ``model``, builds a new
+        ``e3nn.nn.models.gate_points_2101.Network`` model based on those
+        hyperparameters, and copies over the weights. Otherwise, all ``*args`` and
+        ``**kwargs`` are passed directly to the
+        ``e3nn.nn.models.gate_points_2101.Network`` constructor.
+
+        Parameters
+        ----------
+        model : ``e3nn.nn.models.gate_points_2101.Network``
+            e3nn model to use to construct the underlying model
+        """
+        # If no model is passed, construct e3nn model with model_kwargs,
+        #  otherwise copy all parameters and weights over
         if model is None:
             super(E3NN, self).__init__(*args, **kwargs)
             self.model_parameters = kwargs
@@ -30,16 +51,36 @@ class E3NN(Network):
                 "reduce_output": model.reduce_output,
             }
             # These need a bit of work to get
-            # Use last layer bc guaranteed to be present and is just a Convolution
+            # Use last layer bc guaranteed to be present and is just a Convolution (no
+            #  Gate)
             conv = model.layers[-1]
-            model_kwargs["radial_layers"] =  len(conv.fc.hs) - 2
-            model_kwargs["radial_neurons"] =  conv.fc.hs[1]
-            model_kwargs["num_neighbors"] =  conv.num_neighbors
+            model_kwargs["radial_layers"] = len(conv.fc.hs) - 2
+            model_kwargs["radial_neurons"] = conv.fc.hs[1]
+            model_kwargs["num_neighbors"] = conv.num_neighbors
             super(E3NN, self).__init__(**model_kwargs)
             self.model_parameters = model_kwargs
             self.load_state_dict(model.state_dict())
 
     def forward(self, data):
+        """
+        Make a prediction of the target property based on an input structure.
+
+        Parameters
+        ----------
+        data : dict
+            This dictionary should at minimum contain entries for:
+
+            * ``"pos"``: Atom coordinates
+
+            * ``"x"``: One-hot encoding of atomic numbers
+
+            And optionally ``"z"``, which stores the node attributes.
+
+        Returns
+        -------
+        torch.Tensor
+            Model prediction
+        """
         x = super(E3NN, self).forward(data)
         copy = deepcopy(data)
         copy["x"] = torch.clone(x)
@@ -47,23 +88,24 @@ class E3NN(Network):
 
     def _get_representation(self, reduce_output=False):
         """
-        Input model, remove last layer.
+        Copy model and remove last layer.
+
         Parameters
         ----------
         reduce_output: bool, default=False
-            Whether to reduce output across nodes. This should be set to True
+            Whether to reduce output across nodes. This should be set to ``True``
             if you want a uniform size tensor for every input size (eg when
-            using a ConcatStrategy).
+            using a ``ConcatStrategy``)
 
         Returns
         -------
-        E3NN
+        mtenn.conversion_utils.e3nn.E3NN
             Copied e3nn model with the last layer removed
         """
 
-        ## Copy model so initial model isn't affected
+        # Copy model so initial model isn't affected
         model_copy = deepcopy(self)
-        ## Remove last layer
+        # Remove last layer
         model_copy.layers = model_copy.layers[:-1]
         model_copy.reduce_output = reduce_output
 
@@ -71,15 +113,12 @@ class E3NN(Network):
 
     def _get_energy_func(self):
         """
-        Return last layer of the model.
-        Parameters
-        ----------
-        model: e3nn
-            e3nn model
+        Return copy of last layer of the model.
+
         Returns
         -------
         e3nn.nn.models.gate_points_2101.Network
-            Copy of `model`'s last layer
+            Copy of last layer
         """
 
         final_conv = deepcopy(self)
@@ -95,37 +134,39 @@ class E3NN(Network):
 
     def _get_delta_strategy(self):
         """
-        Build a DeltaStrategy object based on the calling model.
+        Build a :py:class:`DeltaStrategy <mtenn.strategy.DeltaStrategy>` object based on
+        the calling model.
 
         Returns
         -------
-        DeltaStrategy
-            DeltaStrategy built from the model
+        mtenn.strategy.DeltaStrategy
+            ``DeltaStrategy`` built from the model
         """
 
         return DeltaStrategy(self._get_energy_func())
 
     def _get_complex_only_strategy(self):
         """
-        Build a ComplexOnlyStrategy object based on the passed model.
+        Build a :py:class:`ComplexOnlyStrategy <mtenn.strategy.ComplexOnlyStrategy>`
+        object based on the calling model.
 
         Returns
         -------
         ComplexOnlyStrategy
-            ComplexOnlyStrategy built from `self`
+            ``ComplexOnlyStrategy`` built from the model
         """
 
         return ComplexOnlyStrategy(self._get_energy_func())
 
     def _get_concat_strategy(self):
         """
-        Build a ConcatStrategy object using the key "x" to extract the tensor
-        representation from the data dict.
+        Build a :py:class:`ConcatStrategy <mtenn.strategy.ConcatStrategy>` object using
+        the key ``"x"`` to extract the tensor representation from the data dict.
 
         Returns
         -------
         ConcatStrategy
-            ConcatStrategy for the model
+            ``ConcatStrategy`` for the model
         """
 
         return ConcatStrategy(extract_key="x")
@@ -142,46 +183,52 @@ class E3NN(Network):
         comb_readout=None,
     ):
         """
-        Exposed function to build a Model object from a E3NN object. If none
-        is provided, a default model is initialized.
+        Exposed function to build a :py:class:`Model <mtenn.model.Model>` or
+        :py:class:`GroupedModel <mtenn.model.GroupedModel>` from an :py:class:`E3NN
+        <mtenn.conversion_utils.e3nn.E3NN>` (or args/kwargs). If no ``model`` is given,
+        use the ``*args`` and ``**kwargs``.
 
         Parameters
         ----------
-        model: E3NN, optional
-            E3NN model to use to build the Model object. If left as none, a
-            default model will be initialized and used
+        model: mtenn.conversion_utils.e3nn.E3NN, optional
+            ``E3NN`` model to use to build the ``Model`` object. If not given, use the
+            passed ``model_kwargs``
         model_kwargs: dict, optional
-            Dictionary used to initialize E3NN model if model is not passed in
+            Dictionary used to initialize ``E3NN`` model if nothing is passed for
+            ``model``
         grouped: bool, default=False
-            Whether this model should accept groups of inputs or one input at a
-            time.
+            Build a ``GroupedModel``
         fix_device: bool, default=False
             If True, make sure the input is on the same device as the model,
-            copying over as necessary.
+            copying over as necessary
         strategy: str, default='delta'
-            Strategy to use to combine representation of the different parts.
-            Options are ['delta', 'concat', 'complex']
-        combination: Combination, optional
-            Combination object to use to combine predictions in a group. A value
-            must be passed if `grouped` is `True`.
-        pred_readout : Readout
-            Readout object for the energy predictions. If `grouped` is `False`,
-            this option will still be used in the construction of the `Model`
-            object.
-        comb_readout : Readout
-            Readout object for the combination output.
+            ``Strategy`` to use to combine representations of the different parts.
+            Options are [``delta``, ``concat``, ``complex``]
+        combination: mtenn.combination.Combination, optional
+            ``Combination`` object to use to combine multiple predictions. A value must
+            be passed if ``grouped`` is ``True``
+        pred_readout : mtenn.readout.Readout, optional
+            ``Readout`` object for the individual energy predictions. If a
+            ``GroupedModel`` is being built, this ``Readout`` will be applied to each
+            individual prediction before the values are passed to the ``Combination``.
+            If a ``Model`` is being built, this will be applied to the single prediction
+            before it is returned
+        comb_readout : mtenn.readout.Readout, optional
+            Readout object for the combined multi-pose prediction, in the case that a
+            ``GroupedModel`` is being built. Otherwise, this is ignored
+
         Returns
         -------
-        Model
-            Model object containing the desired Representation and Strategy
+        mtenn.model.Model
+            ``Model`` or ``GroupedModel`` containing the desired ``Representation``,
+            ``Strategy``, and ``Combination`` and ``Readout`` s as desired
         """
 
-        #
         if model is None:
             model = E3NN(model_kwargs)
 
-        ## Construct strategy module based on model and
-        ##  representation (if necessary)
+        # Construct strategy module based on model and
+        #  representation (if necessary)
         strategy = strategy.lower()
         if strategy == "delta":
             strategy = model._get_delta_strategy()
@@ -195,10 +242,10 @@ class E3NN(Network):
         else:
             raise ValueError(f"Unknown strategy: {strategy}")
 
-        ## Get representation module
+        # Get representation module
         representation = model._get_representation(reduce_output=reduce_output)
 
-        ## Check on `combination`
+        # Check on `combination`
         if grouped and (combination is None):
             raise ValueError(
                 "Must pass a value for `combination` if `grouped` is `True`."
