58d40aa1e5e28ba56d901856507b6de4e11dd773
kaminow <kaminow@cshl.edu>
2024-04-18 12:17:50 -0400

    Update conversion_utils.__init__.

--- mtenn/conversion_utils/__init__.py
+++ mtenn/conversion_utils/__init__.py
@@ -1,10 +1,7 @@
 """
-Subpackage where the actual mtenn wrappers for individual models live.
+Subpackage where the implementations for individual models live. Each module in here
+should have a class that handles the logic for constructing ``Representation`` and
+``Strategy`` blocks for that model. For more information on how this works and how to
+add your own model, see our guides on :ref:`new-installable-model-guide` and
+:ref:`new-non-installable-model-guide`.
 """
-
-from .e3nn import E3NN
-from .gat import GAT
-from .schnet import SchNet
-from .visnet import ViSNet
-
-__all__ = ["E3NN", "GAT", "SchNet", "ViSNet"]
