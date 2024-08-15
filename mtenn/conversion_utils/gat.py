b81ec61305a659d09f52705397a14f38e6627eb6
kaminow <benjamin.kaminow@choderalab.org>
2024-08-15 10:28:58 -0400

    Migrate PyG-GAT -> GAT.

--- mtenn/conversion_utils/gat.py
+++ mtenn/conversion_utils/gat.py
@@ -1,108 +1,47 @@
 """
 ``Representation`` and ``Strategy`` implementations for the graph attention model
-architecture. The underlying model that we use is the implementation in the
-`DGL-LifeSCi <https://lifesci.dgl.ai/api/model.gnn.html#module-dgllife.model.gnn.gat>`_
-package.
+architecture. The underlying model that we use is the implementation in
+`PyTorch Geometric <https://pytorch-geometric.readthedocs.io/en/latest/generated/
+torch_geometric.nn.models.GAT.html>`_.
 """
 from copy import deepcopy
 import torch
-from dgllife.model import GAT as GAT_dgl
-from dgllife.model import WeightedSumAndMax
+from torch_geometric.nn.models import GAT as PygGAT
 
 from mtenn.model import LigandOnlyModel
 
 
 class GAT(torch.nn.Module):
     """
-    ``mtenn`` wrapper around the DGL-LifeSci GAT model. This class handles construction
-    of the model and the formatting into ``Representation`` and ``Strategy`` blocks.
+    ``mtenn`` wrapper around the PyTorch Geometric GAT model. This class handles
+    construction of the model and the formatting into ``Representation`` and
+    ``Strategy`` blocks.
     """
 
     def __init__(self, *args, model=None, **kwargs):
         """
-        Initialize the underlying ``dgllife.model.GAT`` model, as well as the ``mtenn``
-        -specific code on top. If a value is passed for ``model``, builds a new
-        ``dgllife.model.GAT`` model based on those hyperparameters, and copies over the
-        weights. Otherwise, all ``*args`` and ``**kwargs`` are passed directly to the
-        ``dgllife.model.GAT`` constructor.
+        Initialize the underlying ``torch_geometric.nn.models.GAT`` model. If a value is
+        passed for ``model``, builds a new ``torch_geometric.nn.models.GAT`` model based
+        on those hyperparameters, and copies over the weights. Otherwise, all ``*args``
+        and ``**kwargs`` are passed directly to the ``torch_geometric.nn.models.GAT``
+        constructor.
 
         Parameters
         ----------
-        model : ``dgllife.model.GAT``, optional
-            DGL-LifeSci model to use to construct the underlying model
+        model : ``torch_geometric.nn.models.GAT``, optional
+            PyTorch Geometric model to use to construct the underlying model
         """
         super().__init__()
-
-        # First check for predictor_hidden_feats so it doesn't get passed to DGL GAT
-        #  constructor
-        predictor_hidden_feats = kwargs.pop("predictor_hidden_feats", None)
 
         # If no model is passed, construct model based on passed args, otherwise copy
         #  all parameters and weights over
         if model is None:
-            self.gnn = GAT_dgl(*args, **kwargs)
+            self.gnn = PygGAT(*args, **kwargs)
         else:
-            # Parameters that are conveniently accessible from the top level
-            in_feats = model.gnn_layers[0].gat_conv.fc.in_features
-            hidden_feats = model.hidden_feats
-            num_heads = model.num_heads
-            agg_modes = model.agg_modes
-            # Parameters that can only be adcessed layer-wise
-            layer_params = []
-            for l in model.gnn_layers:
-                gc = l.gat_conv
-                new_params = (
-                    gc.feat_drop.p,
-                    gc.attn_drop.p,
-                    gc.leaky_relu.negative_slope,
-                    gc.activation,
-                    bool(gc.res_fc),
-                    (gc.res_fc.bias is not None)
-                    if gc.has_linear_res
-                    else gc.has_explicit_bias,
-                )
-                layer_params += [new_params]
+            self.gnn = deepcopy(model)
 
-            (
-                feat_drops,
-                attn_drops,
-                alphas,
-                activations,
-                residuals,
-                biases,
-            ) = zip(*layer_params)
-            self.gnn = GAT_dgl(
-                in_feats=in_feats,
-                hidden_feats=hidden_feats,
-                num_heads=num_heads,
-                feat_drops=feat_drops,
-                attn_drops=attn_drops,
-                alphas=alphas,
-                residuals=residuals,
-                agg_modes=agg_modes,
-                activations=activations,
-                biases=biases,
-            )
-            self.gnn.load_state_dict(model.state_dict())
-
-        # Copied from GATPredictor class, figure out how many features the last
-        #  layer of the GNN will have
-        if self.gnn.agg_modes[-1] == "flatten":
-            gnn_out_feats = self.gnn.hidden_feats[-1] * self.gnn.num_heads[-1]
-        else:
-            gnn_out_feats = self.gnn.hidden_feats[-1]
-        self.readout = WeightedSumAndMax(gnn_out_feats)
-
-        # Use given hidden feats if supplied, otherwise use 1/2 gnn_out_feats
-        if predictor_hidden_feats is None:
-            predictor_hidden_feats = gnn_out_feats // 2
-
-        # 2 layer MLP with ReLU activation (borrowed from GATPredictor)
-        self.predict = torch.nn.Sequential(
-            torch.nn.Linear(2 * gnn_out_feats, predictor_hidden_feats),
-            torch.nn.ReLU(),
-            torch.nn.Linear(predictor_hidden_feats, 1),
-        )
+        # Predict from mean of node features
+        self.predict = torch.nn.Linear(self.gnn.out_channels, 1)
 
     def forward(self, data):
         """
@@ -110,20 +49,26 @@ class GAT(torch.nn.Module):
 
         Parameters
         ----------
-        data : dict
-            This dictionary should at minimum contain an entry for ``"g"``, which should
-            be the molecule graph representation and will be passed to the underlying
-            ``dgllife.model.GAT`` object
+        data : dict[str, torch.Tensor]
+            This dictionary should at minimum contain entries for:
+
+            * ``"x"``: Atom coordinates, shape of (num_atoms, num_features)
+
+            * ``"edge_index"``: All edges in the graph, shape of (2, num_edges) with the
+            first row giving the source node indices and the second row giving the
+            destination node indices for each edge
 
         Returns
         -------
         torch.Tensor
             Model prediction
         """
-        g = data["g"]
-        node_feats = self.gnn(g, g.ndata["h"])
-        graph_feats = self.readout(g, node_feats)
-        return self.predict(graph_feats)
+        # Run through GNN
+        graph_gred = self.gnn(x=data["x"], edge_index=data["edge_index"])
+        # Take mean of feature values across nodes
+        graph_gred = graph_gred.mean(dim=0)
+        # Make final prediction
+        return self.predict(graph_gred)
 
     def _get_representation(self):
         """
@@ -142,15 +87,15 @@ class GAT(torch.nn.Module):
 
     def _get_energy_func(self):
         """
-        Return last two layer of the model.
+        Return last layer of the model.
 
         Returns
         -------
-        torch.nn.Sequential
-            Sequential module calling copy of `model`'s last two layers
+        torch.nn.Linear
+            Final energy prediction layer of the model
         """
 
-        return torch.nn.Sequential(deepcopy(self.readout), deepcopy(self.predict))
+        return deepcopy(self.readout)
 
     @staticmethod
     def get_model(
