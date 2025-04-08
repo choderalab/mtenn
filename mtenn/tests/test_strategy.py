4747ddd9409a17bdbb13cdcb0ef3be594207f77d
kaminow <benjamin.kaminow@choderalab.org>
2025-04-08 15:05:53 -0400

    Add tests for SplitDeltaStrategy.

--- mtenn/tests/test_strategy.py
+++ mtenn/tests/test_strategy.py
@@ -1,7 +1,12 @@
 import pytest
 import torch
 
-from mtenn.strategy import ComplexOnlyStrategy, ConcatStrategy, DeltaStrategy
+from mtenn.strategy import (
+    ComplexOnlyStrategy,
+    ConcatStrategy,
+    DeltaStrategy,
+    SplitDeltaStrategy,
+)
 
 
 @pytest.fixture
@@ -11,6 +16,28 @@ def energy_func():
     """
     model = torch.nn.Linear(5, 1, bias=False)
     model.load_state_dict({"weight": torch.ones_like(model.weight)})
+
+    return model
+
+
+@pytest.fixture
+def protein_energy_func():
+    """
+    Simple linear model with all weights == 1 for easy testing.
+    """
+    model = torch.nn.Linear(5, 1, bias=False)
+    model.load_state_dict({"weight": torch.ones_like(model.weight) * 2})
+
+    return model
+
+
+@pytest.fixture
+def ligand_energy_func():
+    """
+    Simple linear model with all weights == 1 for easy testing.
+    """
+    model = torch.nn.Linear(5, 1, bias=False)
+    model.load_state_dict({"weight": torch.ones_like(model.weight) * 3})
 
     return model
 
@@ -82,3 +109,44 @@ def test_delta_strat(energy_func, inputs):
     pred = strat(*inputs)
 
     assert pred == ref_value
+
+
+@pytest.mark.parametrize(
+    "use_ligand_energy_func,use_protein_energy_func",
+    [(None, None), (True, None), (None, True), (True, True)],
+)
+def test_split_delta_strat(
+    inputs,
+    energy_func,
+    protein_energy_func,
+    ligand_energy_func,
+    use_ligand_energy_func,
+    use_protein_energy_func,
+):
+    comp, prot, lig = inputs
+
+    ref_value = comp.sum(axis=None)
+    if use_ligand_energy_func:
+        use_ligand_energy_func = ligand_energy_func
+        ref_value -= (3 * lig).sum(axis=None)
+    else:
+        ref_value -= lig.sum(axis=None)
+    if use_protein_energy_func:
+        use_protein_energy_func = protein_energy_func
+        ref_value -= (2 * prot).sum(axis=None)
+    else:
+        ref_value -= prot.sum(axis=None)
+
+    strat = SplitDeltaStrategy(
+        complex_energy_func=energy_func,
+        ligand_energy_func=use_ligand_energy_func,
+        protein_energy_func=use_protein_energy_func,
+    )
+    pred = strat(comp=comp, prot=prot, lig=lig)
+
+    assert pred == ref_value
+
+
+def test_split_delta_strat_needs_complex_energy_func(energy_func):
+    with pytest.raises(ValueError):
+        SplitDeltaStrategy(complex_energy_func=None)
